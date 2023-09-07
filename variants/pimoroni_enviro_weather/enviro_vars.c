242ac002465cf26bf1ce2e3aa09a6f4c54a8457d
Odd Stråbø <oddstr13@openshell.no>
2023-09-07 20:14:19 +0200

    Implement reading of boot reason in early_variant_init

---
+++ variants/pimoroni_enviro_weather/enviro_vars.c
@@ -0,0 +1,3 @@
+#include "enviro_vars.h"
+
+uint32_t enviro_boot_reason_flags = 0;
---
+++ variants/pimoroni_enviro_weather/enviro_vars.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#define ENVIRO_BOOT_REASON_RTC_PIN 8
+#define ENVIRO_BOOT_REASON_RTC_BITS 1
+#define ENVIRO_BOOT_REASON_RTC_OFFSET 0
+
+#define ENVIRO_BOOT_REASON_BUTTON_PIN 7
+#define ENVIRO_BOOT_REASON_BUTTON_BITS 3
+#define ENVIRO_BOOT_REASON_BUTTON_OFFSET 1
+
+#define ENVIRO_BOOT_REASON_RAIN_PIN 10
+#define ENVIRO_BOOT_REASON_RAIN_BITS 7
+#define ENVIRO_BOOT_REASON_RAIN_OFFSET 2
+
+
+#ifndef __ASSEMBLER__
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+extern uint32_t enviro_boot_reason_flags;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __ASSEMBLER__
